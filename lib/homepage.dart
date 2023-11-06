bd557aaec737b7eee2a365681ca7559a5789c6e2
gauransh18 <66783709+gauransh18@users.noreply.github.com>
2023-11-07 00:43:25 +0530

    added 'hello' toast

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,7 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   //creating a reference for our box
 
   final _myBox = Hive.box('myBox');
@@ -23,7 +21,6 @@ class _HomePageState extends State<HomePage> {
     _myBox.put(1, 'Gauransh'); //key value pair
     _myBox.put(2, ['Red', 'Blue', 'Green']);
   }
-
 
   //read data
   void readData() {
@@ -35,6 +32,33 @@ class _HomePageState extends State<HomePage> {
   void deleteData() {
     _myBox.delete(1);
     _myBox.delete(2);
+  }
+
+  //hello hive toast
+  void showToast() {
+    // final snackBar = SnackBar(
+    //   content: Text('Hello hive 👋'),
+    //   duration: Duration(seconds: 1),
+    // );
+    // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+    Fluttertoast.showToast(
+        msg: "Hello Hive 👋",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // WidgetsBinding.instance.addPostFrameCallback((_) {
+      showToast();
+    // });
   }
 
   @override
